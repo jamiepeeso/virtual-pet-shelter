0b2444f43cf04e07d35995ec666023297870c253
Jamie Peeso <jamie.peeso@gmail.com>
2017-09-21 19:22:52 -0400

    Virtual Pet Shelter 09.21.2017 version

---
+++ src/VirtualPet.java
@@ -0,0 +1,10 @@
+
+public class VirtualPet {
+
+}
+//include instance variables representing:
+//	name
+//	description
+//include a constructor that accepts a name and description
+//include a constructor that, in addition to name and description, accepts default values for the pet's attributes 
+//(hunger, boredom, etc)
